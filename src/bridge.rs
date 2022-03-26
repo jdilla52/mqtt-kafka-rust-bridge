e18279f5c3bfd789dd44d4a39dbfe2eb076b09ae
jacques <jacques.perrault@branch.technology>
2022-03-26 19:34:28 -0400

    setup kafka client in bridge

--- src/bridge.rs
+++ src/bridge.rs
@@ -1,16 +1,15 @@
-use std::process;
-use futures::StreamExt;
-use crate::kafka_client::KafkaClient;
+use crate::kafka_client::{send_kafka_message, KafkaClient};
 use crate::mqtt_client::MqttClient;
+use futures::StreamExt;
+use std::process;
 
 struct Bridge {}
 
 impl Bridge {
     pub async fn new() {
-        let mut mqtt_client = MqttClient::new(
-            "tcp://127.0.0.1:1883".to_string(),
-                                     "test".to_string(), );
-            mqtt_client.build_mqtt_connection().await;
+        let mut mqtt_client =
+            MqttClient::new("tcp://127.0.0.1:1883".to_string(), "test".to_string());
+        mqtt_client.build_mqtt_connection().await;
         let valid = mqtt_client.subscribe().await;
 
         let kafka_client = KafkaClient::new("127.0.0.1:9092".into());
@@ -20,20 +19,19 @@ impl Bridge {
         });
 
         while let Some(msg_opt) = stream.next().await {
-
             if let Some(msg) = msg_opt {
+                let l = kafka_client.producer.clone();
                 tokio::spawn(async move {
-                   // kafka_client.send_message("test".into(), "test".into(), msg.payload());
+                    // https://docs.rs/rdkafka/0.28.0/rdkafka/producer/struct.FutureProducer.html
+                    // docs: clone producer to threads
+                    send_kafka_message(&l, "test".into(), "lkjl".into(), "hello kafka".as_bytes());
                 });
-                println!("{}", msg);
-
             }
-            else {
-                // A "None" means we were disconnected. Try to reconnect...
-                println!("Lost connection. Attempting reconnect.");
-                mqtt_client.try_reconnect();
-            }
+            // else {
+            //     // A "None" means we were disconnected. Try to reconnect...
+            //     println!("Lost connection. Attempting reconnect.");
+            //     mqtt_client.try_reconnect();
+            // }
         }
-
     }
 }
--- src/kafka_client.rs
+++ src/kafka_client.rs
@@ -1,21 +1,22 @@
-use std::error::Error;
-use std::convert::TryFrom;
-use std::time::Duration;
 use log::{debug, error};
+use std::convert::TryFrom;
+use std::error::Error;
+use std::time::Duration;
 
-use rdkafka::consumer::{StreamConsumer};
-use rdkafka::{ClientConfig, Message};
 use rdkafka::config::RDKafkaLogLevel;
 use rdkafka::consumer::Consumer;
-use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::consumer::StreamConsumer;
 use rdkafka::producer::future_producer::OwnedDeliveryResult;
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::{ClientConfig, Message};
 
+#[derive(Clone)]
 pub struct KafkaClient {
     brokers: String,
-    producer: FutureProducer,
+    pub(crate) producer: FutureProducer,
 }
 
-impl KafkaClient {
+impl<'a> KafkaClient {
     pub fn new(brokers: String) -> KafkaClient {
         let producer: FutureProducer = ClientConfig::new()
             .set("bootstrap.servers", &brokers)
@@ -24,39 +25,40 @@ impl KafkaClient {
             .expect("Producer creation error");
 
         KafkaClient {
-            brokers,
+            brokers: brokers,
             producer,
         }
     }
-
-    pub async fn send_message(&self, topic: String, key: String, record: &[u8]) -> bool {
-        let record = FutureRecord::to(&topic)
-            .key(&key)
-            .payload(record);
-
-        let produce_future = producer.send(record, Duration::from_millis(1)).await;
-        return match produce_future {
-            Ok(delivery) => {
-                debug!("Sent: {:?}", delivery);
-                true
-            }
-            Err((e, _)) => {
-                error!("Error: {:?}", e);
-                false
-            }
-        };
-    }
 }
 
+pub async fn send_kafka_message(
+    producer: &FutureProducer,
+    topic: String,
+    key: String,
+    record: &[u8],
+) -> bool {
+    let record = FutureRecord::to(&topic).key(&key).payload(record);
+
+    let produce_future = producer.send(record, Duration::from_millis(1)).await;
+    return match produce_future {
+        Ok(delivery) => {
+            debug!("Sent: {:?}", delivery);
+            true
+        }
+        Err((e, _)) => {
+            error!("Error: {:?}", e);
+            false
+        }
+    };
+}
 fn init() {
     let _ = env_logger::builder().is_test(true).try_init();
 }
 
 #[cfg(test)]
 mod tests {
-    use std::any::TypeId;
     use super::*;
-
+    use std::any::TypeId;
 
     fn get_type_of<T: 'static>(_: &T) -> TypeId {
         TypeId::of::<T>()
@@ -65,14 +67,23 @@ mod tests {
     #[test]
     fn test_create_producer() {
         let client = KafkaClient::new("127.0.0.1:9092".into());
-        assert_eq!(TypeId::of::<FutureProducer>(), get_type_of(&client.producer));
+        assert_eq!(
+            TypeId::of::<FutureProducer>(),
+            get_type_of(&client.producer)
+        );
     }
 
     #[tokio::test]
     async fn test_message() {
         init();
         let j = KafkaClient::new("127.0.0.1:9092".into());
-        let out = j.send_message("test".to_string(), "test".to_string(), "hello kafka".as_bytes()).await;
+        let out = send_kafka_message(
+            &j.producer,
+            "test".to_string(),
+            "test".to_string(),
+            "hello kafka".as_bytes(),
+        )
+        .await;
         assert!(out);
     }
 }

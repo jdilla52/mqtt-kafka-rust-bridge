afd370f89cc08f8842a40e9160801c0a66a4db1c
jacques <jacques.perrault@branch.technology>
2022-03-27 14:06:12 -0400

    added map route

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,14 +1,11 @@
 use crate::config::HttpSettings;
-use actix_web::{
-    error, get,
-    http::{
-        header::{self, ContentType},
-        Method, StatusCode,
-    },
-    middleware, web, App, Either, HttpRequest, HttpResponse, HttpServer, Responder, Result,
-};
+use actix_web::{error, get, http::{
+    header::{self, ContentType},
+    Method, StatusCode,
+}, middleware, web, App, Either, HttpRequest, HttpResponse, HttpServer, Responder, Result, http};
 use std::net;
 use std::os::unix::net::SocketAddr;
+use serde_json::json;
 
 struct BridgeHttpServer {
     settings: HttpSettings,
@@ -19,6 +16,16 @@ async fn default_handler(req_method: Method) -> Result<impl Responder> {
         Method::GET => Ok(Either::Left(HttpResponse::Forbidden().finish())),
         _ => Ok(Either::Right(HttpResponse::MethodNotAllowed().finish())),
     }
+}
+
+async fn site_map() -> HttpResponse{
+    HttpResponse::Ok()
+    // .append_header((http::header::CONTENT_TYPE, "application/json"))
+        .json(json!({
+                "self": "/",
+                "Healthcheck": "/healthcheck",
+                "Bridge Status": "/stats"
+    }))
 }
 
 async fn health_check() -> HttpResponse {
@@ -41,15 +48,17 @@ impl BridgeHttpServer {
             App::new()
                 // .servce()
                 .route("/healthcheck", web::get().to(health_check))
+                .route("/", web::get().to(health_check))
                 // default
                 .default_service(web::to(default_handler))
         })
-        .bind(settings.address)?
-        .workers(1)
-        .run()
-        .await
+            .bind(settings.address)?
+            .workers(1)
+            .run()
+            .await
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -57,6 +66,10 @@ mod tests {
     use crate::http_server;
     use futures::task::Spawn;
     use std::any::TypeId;
+    use std::hash::Hasher;
+    use actix_web::body::to_bytes;
+    use futures::TryFutureExt;
+
     #[tokio::test]
     async fn test_default() {
         let s = BridgeHttpServer {
@@ -69,4 +82,11 @@ mod tests {
         let response: HttpResponse = health_check().await;
         assert!(response.status().is_success())
     }
+
+    #[tokio::test]
+    async fn site_map_check_succeeds() {
+        let response = site_map().await;
+        assert!(response.status().is_success());
+    }
 }
+

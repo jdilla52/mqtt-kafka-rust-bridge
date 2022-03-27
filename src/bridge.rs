9432b9136694cdf8b19a70e37fa71537b3182e55
jacques <jacques.perrault@branch.technology>
2022-03-26 20:31:19 -0400

    bridge sends basic messages

--- src/bridge.rs
+++ src/bridge.rs
@@ -2,8 +2,11 @@ use crate::kafka_client::{send_kafka_message, KafkaClient};
 use crate::mqtt_client::MqttClient;
 use futures::StreamExt;
 use std::process;
+use std::time::Duration;
+use log::{debug, error};
+use rdkafka::producer::FutureRecord;
 
-struct Bridge {}
+pub struct Bridge {}
 
 impl Bridge {
     pub async fn new() {
@@ -24,14 +27,41 @@ impl Bridge {
                 tokio::spawn(async move {
                     // https://docs.rs/rdkafka/0.28.0/rdkafka/producer/struct.FutureProducer.html
                     // docs: clone producer to threads
-                    send_kafka_message(&l, "test".into(), "lkjl".into(), "hello kafka".as_bytes());
+                    send_kafka_message(l, "test".into(), "lkjl".into(), msg.payload()).await;
                 });
             }
-            // else {
-            //     // A "None" means we were disconnected. Try to reconnect...
-            //     println!("Lost connection. Attempting reconnect.");
-            //     mqtt_client.try_reconnect();
-            // }
+            else {
+                // A "None" means we were disconnected. Try to reconnect...
+                println!("Lost connection. Attempting reconnect.");
+                // mqtt_client.try_reconnect();
+            }
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::any::TypeId;
+    use crate::utils::init;
+
+    fn get_type_of<T: 'static>(_: &T) -> TypeId {
+        TypeId::of::<T>()
+    }
+
+
+    #[tokio::test]
+    async fn test_bridge_message() {
+        init();
+        let j = Bridge::new().await;
+    }
+
+    // #[test]
+    // fn test_create_producer() {
+    //     let client = KafkaClient::new("127.0.0.1:9092".into());
+    //     assert_eq!(
+    //         TypeId::of::<FutureProducer>(),
+    //         get_type_of(&client.producer)
+    //     );
+    // }
 }
